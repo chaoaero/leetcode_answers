968495542a920f5c55f9d0efe6bfbee2aef938be
chaoaero <mengweichao999@gmail.com>
2015-11-04 10:30:27 +0800

    add heap sort code

--- merge_k_sorted_list.cc
+++ merge_k_sorted_list.cc
@@ -8,15 +8,156 @@
  *
  ================================================================*/
 #include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<queue>
 using namespace std;
 typedef struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x = 0) : val(x), next(NULL) {}
 } ListNode;
+
+typedef ListNode* heap_elem_t;
+
+typedef struct heap_t {
+    int size;
+    int capacity;
+    heap_elem_t *elems;
+    int (*cmp)(const heap_elem_t*, const heap_elem_t*);
+} heap_t;
+
+int cmp_list_node(const ListNode* a, const ListNode *b) {
+    const int sub = a->val - b->val;
+    if(sub > 0) {
+        return 1;
+    } else if(sub < 0){
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+heap_t* heap_create(const int capacity, int (*cmp)(const heap_elem_t*, const heap_elem_t*)) {
+    heap_t *h = (heap_t*)malloc(sizeof(heap_t));
+    h->size = 0;
+    h->capacity = capacity;
+    h->elems = (heap_elem_t*)malloc(capacity * sizeof(heap_elem_t));
+    h->cmp = cmp;
+}
+
+void heap_destroy(heap_t *h) {
+    free(h->elems);
+    free(h);
+}
+
+int heap_empty(const heap_t *h) {
+    return h->size == 0;
+}
+
+int heap_size(const heap_t *h) {
+    return h->size;
+}
+
+void heap_sift_down(const heap_t *h, const int start) {
+    int i = start;
+    int j;
+    const heap_elem_t tmp  = h->elems[start];
+    for(j = 2 * i + 1; j < h->size; j = 2 * j + 1) {
+        if(j < (h->size - 1) && h->cmp(&(h->elems[j]), &(h->elems[j + 1]) ) > 0) {
+            j++;
+        }
+        if(h->cmp(&tmp, &(h->elems[j])) <= 0) {
+            break;
+        } else {
+            h->elems[i] = h->elems[j];
+            i = j;
+        }
+    }
+    h->elems[i] = tmp;
+}
+
+void heap_sift_up(const heap_t *h, const int start) {
+    int j = start;
+    int i = j / 2;
+    const heap_elem_t tmp = h->elems[start];
+    while(j > 0) {
+        if(h->cmp(&(h->elems[j]), &tmp) <= 0 ) {
+            break;
+        } else {
+            h->elems[j] = h->elems[i];
+            j = i;
+            i = (i - 1) / 2;
+        }
+        h->elems[j] = tmp;
+
+    }
+}
+
+void heap_push(heap_t *h, const heap_elem_t x) {
+    if(h->size == h->capacity) {
+        heap_elem_t* tmp = (heap_elem_t*)realloc(h->elems, h->capacity * 2 * sizeof(heap_elem_t));
+        h->elems = tmp;
+        h->capacity *= 2;
+    }
+    h->elems[h->size] = x;
+    h->size++;
+    heap_sift_up(h, h->size - 1);
+}
+
+void heap_pop(heap_t *h) {
+    h->elems[0] = h->elems[h->size - 1];
+    h->size--;
+    heap_sift_down(h, 0);
+}
+
+heap_elem_t heap_top(const heap_t *h) {
+    return h->elems[0];
+}
+
+
 class Solution {
     public:
+        class Compare {
+            public:
+                bool operator() (const ListNode* a, const ListNode* b) {
+                    if(a->val > b->val) {
+                        return true;
+                    } else if(a->val < b->val) {
+                        return false;
+                    } else
+                        return false;
+                }
+        };
         ListNode* mergeKLists(vector<ListNode*>& lists) {
+            int size = lists.size();
+            if(size == 0 )
+                return NULL;
+            else if(size == 1) {
+                return lists.front();
+            }
+
+            ListNode *head = new ListNode(-1);
+            ListNode *pre, *tmp, *next;
+            pre = head;
+            priority_queue<ListNode*, vector<ListNode*> , Compare> pq;
+            for(vector<ListNode*>::const_iterator iter = lists.begin(); iter != lists.end(); iter++) {
+                if((*iter) != NULL)
+                    pq.push(*iter);
+            }
+            while(!pq.empty()) {
+                tmp = pq.top();
+                next = tmp->next;
+                pre->next = tmp;
+                tmp->next = NULL;
+                pre = tmp;
+                pq.pop();
+                if(next != NULL)
+                    pq.push(next);
+            }
+            tmp = head->next;
+            delete head;
+            return tmp;
 
         }
         ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
@@ -67,7 +208,19 @@ int main() {
         tag->next = p;
         tag = tag->next;
     }
-
-    sol.printList(head);    
-
+    ListNode *new_head = new ListNode(2);
+    tag = new_head;
+    for(int i=2;i<=8; i++) {
+        ListNode *p = new ListNode(i);
+        tag->next = p;
+        tag = tag->next;
+    }
+    vector<ListNode*> woca;
+    sol.printList(head);
+    sol.printList(new_head);
+    woca.push_back(head);
+    woca.push_back(new_head);
+    ListNode *ff = sol.mergeKLists(woca);
+    sol.printList(ff);    
+    return 0;
 }
